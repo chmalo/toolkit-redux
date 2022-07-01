7d485ff06073f44d9a918730e848d447719590c4
Chmalo <chmalo.f@gmail.com>
2022-07-01 08:38:16 -0400

    feat todoId

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -1,10 +1,21 @@
-import {useGetTodosQuery} from "./store/apis";
+import {useGetTodoByIdQuery, useGetTodosQuery} from "./store/apis";
+import {useState} from "react";
 
 export const TodoApp = () => {
 
+    const [todoId, setTodoId] = useState(1);
+
     // @ts-ignore
-    const {data: todos = [], isLoading} = useGetTodosQuery();
-    console.log(todos, isLoading)
+    // const {data: todos = [], isLoading} = useGetTodosQuery();
+    const {data: todo, isLoading} = useGetTodoByIdQuery(todoId);
+
+    const nexTodo = () => {
+        setTodoId(todoId + 1)
+    }
+
+    const prevTodo = () => {
+        setTodoId(todoId - 1)
+    }
 
 
     return (
@@ -14,18 +25,22 @@ export const TodoApp = () => {
 
             <h4>IsLoading: {isLoading ? 'True' : 'False'}</h4>
 
-            <pre>...</pre>
-            <ul>
-                {todos.map((todo: any) => (
-                    <li key={todo.id}>
-                        <strong>{todo.completed ? 'DONE ' : 'Pending '}</strong>
-                        {todo.title}
-                    </li>
-                ))}
-            </ul>
+            <pre>{JSON.stringify(todo)}</pre>
+            {/*<ul>*/}
+            {/*    {todos.map((todo: any) => (*/}
+            {/*        <li key={todo.id}>*/}
+            {/*            <strong>{todo.completed ? 'DONE ' : 'Pending '}</strong>*/}
+            {/*            {todo.title}*/}
+            {/*        </li>*/}
+            {/*    ))}*/}
+            {/*</ul>*/}
 
 
-            <button>
+            <button onClick={prevTodo}>
+                Prev Todo
+            </button>
+
+            <button onClick={nexTodo}>
                 Next Todo
             </button>
         </>
